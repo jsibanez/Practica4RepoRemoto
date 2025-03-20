4dae23b44afc0e2f3c7b6a49e3fd5faeab04d853
jsibanez <jsibanez84@gmail.com>
2025-03-20 19:33:37 +0100

    Version 1.0

---
+++ Práctica4Entornos/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Práctica4Entornos {
+}
---
+++ Práctica4Entornos/src/paquetePrincipal/Clase_v1.java
@@ -0,0 +1,7 @@
+package paquetePrincipal;
+
+public class Clase_v1 {
+	public static void main(String[] args) {
+		System.out.println("Esto será la versión v1.0");
+	}
+}
