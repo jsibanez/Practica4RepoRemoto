55e9e4fc62e356150f550a8d5895c5330a69d64c
jsibanez <jsibanez84@gmail.com>
2025-03-20 19:41:16 +0100

    Versión 1.1

--- Práctica4Entornos/src/paquetePrincipal/Clase_v1.java
+++ Práctica4Entornos/src/paquetePrincipal/Clase_v1.java
@@ -2,6 +2,6 @@ package paquetePrincipal;
 
 public class Clase_v1 {
 	public static void main(String[] args) {
-		System.out.println("Esto será la versión v1.0");
+		System.out.println("Esto será la versión v1.1");
 	}
 }
