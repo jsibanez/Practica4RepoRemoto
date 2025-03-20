7476b178fea5b2e38d3bd6a889388b7a996421be
jsibanez <jsibanez84@gmail.com>
2025-03-20 19:39:33 +0100

    Version nuevo desarrollo

---
+++ Práctica4Entornos/src/paqueteFuncionalidades/Clase_NuevaFuncionalidad.java
@@ -0,0 +1,7 @@
+package paqueteFuncionalidades;
+
+public class Clase_NuevaFuncionalidad {
+	public static void main(String[] args) {
+		System.out.println("Nueva funcionalidad en la rama de desarrollo");
+	}
+}
